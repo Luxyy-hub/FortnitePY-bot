c59bc0db520a4323d95805a999cd4e80ecd849f1
Project-Lucifer <96788441+Project-Lucifer@users.noreply.github.com>
2021-12-29 15:02:12 +0100

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,16 @@
+{
+    "email": "",
+    "password": "",
+    "cid": "CID_001_Athena_Commando_F_Default",
+    "bid": "BID_004_BlackKnight",
+    "pid": "Pickaxe_Lockjaw",
+    "banner": "otherbanner28",
+    "banner_color": "defaultcolor22",
+    "level": "666",
+    "bp_tier": "666",
+    "status": "Lucifer Lobby Bot - Made by Lucynda",
+    "platform": "IOS",
+    "friendaccept": "True",
+    "joinoninvite": "False",
+    "AdminPassword": "1111"
+}
